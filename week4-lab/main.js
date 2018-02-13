80391b5ed7cfe2a70ae6408423220dffab0e0206
hawever9 <anwenh@andrew.cmu.edu>
2018-02-13 11:20:21 -0500

    adding javascript

--- week4-lab/main.js
+++ week4-lab/main.js
@@ -2,3 +2,25 @@ function addNewList() {
     alert('hello world alert!');
     console.log('hello world console');
 }
+
+function addListItem() {
+    console.log('hello world');
+    var list = document.getElementById("grocery-list");
+    var itemInput = document.getElementById("new-list-item");
+    var newItem = document.createElement("li");
+    newItem.appendChild(document.createTextNode(itemInput.value));
+    list.appendChild(newItem);
+}
+
+function deleteListItem(item) {
+        item.parentNode.removeChild(item);
+}
+
+
+function completeListItem(item) {
+    if (item.checked) {
+        item.parentNode.style.textDecoration = "line-through";
+    } else {
+        item.parentNode.style.textDecoration = "none";
+    }
+}

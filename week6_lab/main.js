ccaca10a9ef2e484f7be15d3a17d9b5594d9a514
hawever9 <anwenh@andrew.cmu.edu>
2018-02-20 11:16:17 -0500

    lab6

--- week6_lab/main.js
+++ week6_lab/main.js
@@ -24,3 +24,24 @@ function completeListItem(item) {
         item.parentNode.style.textDecoration = "none";
     }
 }
+
+
+$(document).on("click", "#add-item", function(){
+    var list = $("#grocery-list"); 
+    var itemInput = $("#new-list-item"); 
+    list.append("<li>" + itemInput.val() + "<button class='delete-item'>X</button></li>"); 
+});
+
+
+$(document).on("click", ".delete-item", function(){
+    $(this).parent().remove();
+});
+
+$(document).on("click", ".complete-item", function(){
+    if ($(this).is(':checked')) {
+        $(this).parent().css("textDecoration", "line-through");
+    } else {
+        $(this).parent().css("textDecoration", "none");
+    }
+});
+
